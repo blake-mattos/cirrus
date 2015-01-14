a909255f3ed6409cee1a6f53093eebc1d2111cf3
blake-mattos <blakesamplemattos@gmail.com>
2015-01-13 22:41:45 -0800

    set genre to folk

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ SC.initialize({
 });
 
 $(document).ready(function() {
-  SC.get('/tracks', { genres: 'lesbian acid folk' }, function(tracks) {
+  SC.get('/tracks', { genres: 'folk' }, function(tracks) {
     $(tracks).each(function(index, track) {
       $('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + track.artwork_url + '">'));
     });
