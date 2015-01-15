dffd3cb04c8455cd1fe1cd2f94b58ac159b0ef75
blake-mattos <blakesamplemattos@gmail.com>
2015-01-14 16:37:57 -0800

    got song bg working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 SC.initialize({
-  client_id: '781aa72f47d54e21fc84ec186d504e1c'
+	client_id: '781aa72f47d54e21fc84ec186d504e1c'
 });
 
 $(document).ready(function() {
-  SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-    	var artUrl = track.artwork_url;
-    	var bigArt = artUrl.replace("large", "t500x500");
-    	console.log(bigArt);
-      $('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
-    });
-  });
+	SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
+		$(tracks).each(function(index, track) {
+			var artUrl = track.artwork_url;
+			var bigArt = artUrl.replace("large", "t500x500");
+			$('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
+		});
+	});
+	function setBG() {
+		$('#bigWrap').css('background-image', 'url(https://i1.sndcdn.com/artworks-000058493054-vcrifw-t500x500.jpg)');
+	};
+	setBG();
 });
