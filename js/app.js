60bfeb8fd3fa86560fd5db3f8fdbedc8b22393b0
blake-mattos <blakesamplemattos@gmail.com>
2015-01-16 09:41:02 -0800

    got the player working w00t

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
-SC.initialize({
-	client_id: '781aa72f47d54e21fc84ec186d504e1c'
-});
 
 $(document).ready(function() {
-	SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
-		$(tracks).each(function(index, track) {
-			var artUrl = track.artwork_url;
-			var bigArt = artUrl.replace("large", "t500x500");
-			$('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
-		});
+
+	SC.initialize({
+		client_id: '781aa72f47d54e21fc84ec186d504e1c'
 	});
-	function setBG() {
-		$('#bg').css('background-image', 'url(https://i1.sndcdn.com/artworks-000058493054-vcrifw-t500x500.jpg)');
-	};
-	setBG();
+
+
+	function playSomeSound(genre) {
+		SC.get('/tracks', {
+			genres: genre,
+		}, function(tracks) { 
+			var random = Math.floor(Math.random() * 49);
+			SC.oEmbed(tracks[random].uri, { autoplay: true }, document.getElementById('player'));
+		});
+	}
+
+	var menuLinks = document.getElementsByClassName('genre');
+	console.log(menuLinks)
+	for (var i = 0; i < menuLinks.length; i++) {
+		var menuLink = menuLinks[i];
+		menuLink.onclick = function(e) {
+			e.preventDefault();
+			playSomeSound(menuLink.innerHTML);
+		};
+	}
 });
+
+
+
+
+
+// SC.initialize({
+// 	client_id: '781aa72f47d54e21fc84ec186d504e1c'
+// });
+
+// $(document).ready(function() {
+// 	// SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
+// 	// 	$(tracks).each(function(index, track) {
+// 	// 		var artUrl = track.artwork_url;
+// 	// 		var bigArt = artUrl.replace("large", "t500x500");
+// 	// 		$('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
+// 	// 	});
+// 	// });
+// // 	setBG();
+// // }
