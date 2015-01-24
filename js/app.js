9b694c1d20d13ebdf7e0a56da87d298468535d6a
blake-mattos <blakesamplemattos@gmail.com>
2015-01-23 21:35:04 -0800

    starting to get album art

--- js/app.js
+++ js/app.js
@@ -5,37 +5,34 @@ $(document).ready(function() {
 		client_id: '781aa72f47d54e21fc84ec186d504e1c'
 	});
 
-	function playSomeSound(genre) {
-		SC.get('/tracks', {
-			genres: genre,
-		}, function(tracks) { 
-			//SoundCloud return 50 tracks by default, this picks one of them at random
-			var random = Math.floor(Math.random() * 49);
-			SC.oEmbed(tracks[random].uri, { autoplay: true }, document.getElementById('player'));
-		});
-	};
+	// function playSomeSound(genre) {
+	// 	SC.get('/tracks', {
+	// 		genres: genre,
+	// 	}, function(tracks) { 
+	// 		//SoundCloud return 50 tracks by default, this picks one of them at random
+	// 		var random = Math.floor(Math.random() * 49);
+	// 		SC.oEmbed(tracks[random].uri, { autoplay: true }, document.getElementById('player'));
+	// 	});
+	// };
 
-	  $('.genre').click(function(){
+	$('.genre').click(function(){
 		var selectedGenre = $(this).text();
 		playSomeSound(selectedGenre);
-				console.log(selectedGenre);
-	  });
+		console.log(selectedGenre);
+	});
 
-	// var menuLinks = document.getElementsByClassName('genre');
-	// console.log(menuLinks);
+function setBG(bigArt) {
+	$('body').css("background-color","blue");
+};
 
-	// for (var i = 0; i < menuLinks.length; i++) {
-	// 	var menuLink = menuLinks[i];
-	// 	menuLink.onclick = function(e) {
-	// 		e.preventDefault();
-	// 		playSomeSound(menuLink.innerHTML);
-	// 		console.log(menuLink.innerHTML);
-	// 	};
-	// }
-
-	// $('#showGenre').on('click', function() {
-	// console.log(genre);
-	// })
+	SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
+		$(tracks).each(function(index, track) {
+			var artUrl = track.artwork_url;
+			var bigArt = artUrl.replace("large", "t500x500");
+			// $('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
+console.log(bigArt);
+		});
+	});
 
 });
 
