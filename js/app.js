5cb2afde6b66092ed7be26e37dd622712f6dfbdc
blake-mattos <blakesamplemattos@gmail.com>
2015-01-24 21:11:41 -0800

    revert to working

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ function getTracks(genre) {
 
 			//SoundCloud returns 50 tracks by default, this picks one of them at random
 			var random = Math.floor(Math.random() * 49);
-			$('#results').append($('<li></li>').html(track[random].permalink_url));
+			$('#results').append($('<li></li>').html(track.permalink_url));
 		});
 	});
 };
