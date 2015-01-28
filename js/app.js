b1f3daae97eaf27bc91bab0480e89637abe74580
blake-mattos <blakesamplemattos@gmail.com>
2015-01-27 22:04:45 -0800

    tidying up

--- js/app.js
+++ js/app.js
@@ -1,129 +1,92 @@
-
 $(document).ready(function() {
 
-	SC.initialize({
-		client_id: '781aa72f47d54e21fc84ec186d504e1c'
-	});
+    SC.initialize({
+        client_id: '781aa72f47d54e21fc84ec186d504e1c'
+    });
 
-// what does it need to do?
-// user selects a genre -> picks a random track -> autoplay & display art in bg
+    // what does it need to do?
+    // user selects a genre -> picks a random track -> autoplay & display art in bg
 
-//select genre -----/
+    //select genre -----/
 
-//set selectedGenre as global var
-var selectedGenre = ('please choose a genre');
-var theTrack;
+    //set selectedGenre as global var
+    var selectedGenre = ('please choose a genre');
+    var theTrack;
 
-//choose a genre and set it to 'selectedGenre'
-$('.genre').click(function() {
-		//set selectedGenre to what user clicks on
-		selectedGenre = $(this).text();
-		// getArt();
-		// playSomeSound(selectedGenre);
-		console.log('selected genre: ' + selectedGenre);
-		getTracks(selectedGenre);
-	});
+    //choose a genre and set it to 'selectedGenre'
+    $('.genre').click(function() {
+        //set selectedGenre to what user clicks on
+        selectedGenre = $(this).text();
+        // getArt();
+        // playSomeSound(selectedGenre);
+        console.log('selected genre: ' + selectedGenre);
+        getTracks(selectedGenre);
+    });
 
-//pick random track from genre -----/
+    //pick random track from genre -----/
 
-//return tracks from selected genre, choose one at random & set theTrack to the chosen track
-function getTracks(genre) {
-	console.log('getTracks started:' +selectedGenre);
-	SC.get('/tracks', { genres: genre }, function(tracks) {
+    //return tracks from selected genre, choose one at random & set theTrack to the chosen track
+    function getTracks(genre) {
+        console.log('getTracks started:' + selectedGenre);
+        SC.get('/tracks', {
+            genres: genre
+        }, function(tracks) {
 
-		// 	//SoundCloud returns 50 tracks by default, this picks one of them at random
-		var random = Math.floor(Math.random() * 49);
+            // 	//SoundCloud returns 50 tracks by default, this picks one of them at random
+            var random = Math.floor(Math.random() * 49);
 
-		theTrack = tracks[random];
-		playTrack(theTrack.permalink_url);
-		setBG(theTrack.artwork_url);
+            theTrack = tracks[random];
+            playTrack(theTrack.permalink_url);
+            setBG(theTrack.artwork_url);
 
-		console.log('Now Playing: ' +theTrack.title);
-	});
-};
+            console.log('Now Playing: ' + theTrack.title);
+        });
+    };
 
-//autoplay & display art in bg -----/
+    //autoplay & display art in bg -----/
 
-// play theTrack in player
-function playTrack(trackUrl) {
-	SC.oEmbed(trackUrl, {auto_play: true}, document.getElementById('player'));
-};
+    // play theTrack in player
+    function playTrack(trackUrl) {
+        SC.oEmbed(trackUrl, {
+            auto_play: true
+        }, document.getElementById('player'));
+    };
 
-//set art as playerWrap bg
-function setBG(artUrl) {
-	var bigArt = artUrl.replace("large", "t500x500");
-	$('#playerWrap').css("background-image", 'url(' + bigArt + ')');
-	console.log(bigArt);
-};
+    //set art as playerWrap bg
+    function setBG(artUrl) {
+        var bigArt = artUrl.replace("large", "t500x500");
+        $('#playerWrap').css("background-image", 'url(' + bigArt + ')');
+        console.log(bigArt);
+    };
+    console.log(selectedGenre);
 
-console.log(selectedGenre);
+    function getArt() {
+        SC.get('/tracks', {
+            genres: 'ambient'
+        }, function(tracks) {
+            $(tracks).each(function(index, track) {
+                var artUrl = track.artwork_url;
+                var bigArt = artUrl.replace("large", "t500x500");
+                $('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
+            });
+        });
+    };
 
-// //player
-// function playSomeSound(genre) {
-// 	SC.get('/tracks', {
-// 		genres: genre,
-// 	}, function(tracks) { 
-// 			//SoundCloud return 50 tracks by default, this picks one of them at random
-// 			var random = Math.floor(Math.random() * 49);
-// 			SC.oEmbed(tracks[random].uri, { autoplay: true }, document.getElementById('player'));
-// 		});
-// };
+    $("#startPlayer").on('click', function() {
+        $("#splashWrap").fadeOut('fast');
+        $("#selectorWrap").fadeIn('fast');
+        console.log('splashWrap should go away');
+    });
 
-function getArt() {
-	SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
-		$(tracks).each(function(index, track) {
-			var artUrl = track.artwork_url;
-			var bigArt = artUrl.replace("large", "t500x500");
-			$('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
-		});
-	});
-};
+    $(".logo").on('click', function() {
+        $("#splashWrap").show();
+        $('#selectorWrap').hide();
+        $('#playerWrap').hide();
+        console.log('home');
+    });
 
-
-$("#startPlayer").on('click', function() {
-	$("#splashWrap").fadeOut('fast');
-	$("#selectorWrap").fadeIn('fast');
-	console.log('splashWrap should go away');
+    $(".genre").on('click', function() {
+        $('#selectorWrap').hide();
+        $('#playerWrap').show();
+    });
 });
-
-$(".logo").on('click', function() {
-	$("#splashWrap").show();
-	$('#selectorWrap').hide();
-	$('#playerWrap').hide();
-	console.log('home');
-});
-
-$(".genre").on('click', function() {
-	$('#selectorWrap').hide();
-	$('#playerWrap').show();
-});
-
-// function selectedArt() {™	
-// 	console.log('selectedArt: ' +selectedArt);
-// 	SC.get('/tracks', { genres: selectedGenre }, function(tracks) {
-// 		$(tracks).each(function(index, track) {
-// 			var artUrl = track.artwork_url;
-// 			var bigArt = artUrl.replace("large", "t500x500");
-// 			// $('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
-// 			console.log(bigArt);
-// 		});
-// 	});
-// };
-
-});
-
-
-// SC.initialize({
-// 	client_id: '781aa72f47d54e21fc84ec186d504e1c'
-// });
-
-// $(document).ready(function() {
-// 	SC.get('/tracks', { genres: 'ambient' }, function(tracks) {
-// 		$(tracks).each(function(index, track) {
-// 			var artUrl = track.artwork_url;
-// 			var bigArt = artUrl.replace("large", "t500x500");
-// 			$('#results').append($('<li></li>').html(track.title + ' - ' + '<img src="' + bigArt + '">'));
-// 		});
-// 	});
-// 	setBG();
-// }
